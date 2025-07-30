7a99176ad9ddb8e639a22f26ee1352e13b0b9cfa
Rickoroxd <0954744487zaz@gmail.com>
2025-07-30 16:00:01 +0700

    setting

---
+++ enc_dec.cpp
@@ -0,0 +1,11 @@
+#include "enc_dec.h"
+#include <string>
+using namespace std;
+
+string enc(string input){
+    return "ENC_TEST";
+}
+
+string dec(string input){
+    return "DEC_TEST";
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
-
+#include<string>
+#include "enc_dec.h"
 using namespace std;
 int main(){
-    
+    string ans = "UU";
+    cout<< dec(ans);
     return 0;
 }
